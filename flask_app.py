2c1a25007de0995aafeb45a71ee263c04fa67961
Gustavo de Oliveira Martins <69735331+devGustavoMartins@users.noreply.github.com>
2025-08-21 00:44:48 -0300

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,14 +1,13 @@
-from flask import Flask, request, make_response, redirect, abort
+from flask import Flask, request, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return
-    """
+    return f"""
     <h1>Avaliação contínua: Aula 030</h1>
     <ul>
-        <li><a href="{url_for('home')}">Home</a></li>
+        <li><a href="{url_for('main')}">Home</a></li>
         <li><a href="{url_for('identification', name='Gustavo de Oliveira Martins', pt='PT3031772', institution='IFSP')}">Identificação</a></li>
         <li><a href="{url_for('contextrequest')}">Contexto da requisição</a></li>
     </ul>
@@ -16,15 +15,14 @@ def main():
 
 @app.route("/user/<name>/<pt>/<institution>")
 def identification(name, pt, institution):
-    return
-    """
+    return f"""
     <h1>Avaliação contínua: Aula 030</h1>
 
     <h2><b>Aluno:</b> {name}</h2>
     <h2><b>Prontuário:</b> {pt}</h2>
     <h2><b>Instituição:</b> {institution}</h2>
 
-    <p><a href="{url_for('home')}">Voltar</a></p>
+    <p><a href="{url_for('main')}">Voltar</a></p>
     """
 
 @app.route("/contextorequisicao")
@@ -33,13 +31,15 @@ def contextrequest():
     remote_ip = request.remote_addr or "desconhecido"
     host = request.host
 
-    return 
-    """
+    return f"""
     <h1>Avaliação contínua: Aula 030</h1>
 
     <h2><b>Seu navegador é: {user_agent}</b></h2>
     <h2><b>O IP do computador remoto é: {remote_ip}</b></h2>
     <h2><b>O host da aplicação é: {host}</b></h2>
 
-    <p><a href="{url_for('home')}">Voltar</a></p>
+    <p><a href="{url_for('main')}">Voltar</a></p>
     """
+
+if __name__ == "__main__":
+    app.run(debug=True)
